64aad07cf47370eb8a5d4ebc0a10d049cec628c9
saraivargas <130935489+saraivargas@users.noreply.github.com>
2023-12-21 20:20:59 -0800

    hello

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -3,35 +3,25 @@
 using namespace std;
 
 class Vehicle {
-   protected:
-    string brand;
-    string color;
-    int speed;
+    protected:
+        string brand;
+        string color;
+        int speed;
 
     public:
-    virtual toString show() = 0;
+        virtual toString show() = 0;
+};
 
 class Car: public Vehicle {
-    public:
+    private:
         int numDoors;
-        void setbType(int bType) {
-      bType = bType;
 
-class Bike: public Vehicle {
     public:
-        string bType;
-
-class Truck: public Vehicle {
-    public:
-        string loadC;
-
+        string toString() {
+            return "Car - Brand: Toyota, Color: Red, Max Speed: 180, Doors: 4";
+        }
 };
 
 int main() {
-    Programmer myObj;
-     myObj.setSalary(50000);
-     myObj.bonus = 15000;
-     cout << "Salary: " << myObj.getSalary() << "\n";
-    cout << "Bonus: " << myObj.bonus << "\n";
     return 0;
 }
