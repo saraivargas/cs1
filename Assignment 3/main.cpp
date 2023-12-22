8d81726262bafd6f216d3750e66e730d1d697303
Sarai Vargas <vargas1sarai@gmail.com>
2023-12-21 21:00:41 -0800

    Working on #2 bullet 3

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -3,25 +3,35 @@
 using namespace std;
 
 class Vehicle {
-    protected:
-        string brand;
-        string color;
-        int speed;
+ protected:
+  string brand;
+  string color;
+  int speed;
 
-    public:
-        virtual toString show() = 0;
+ public:
+  virtual string toString() = 0;
 };
 
-class Car: public Vehicle {
-    private:
-        int numDoors;
+class Car : public Vehicle {
+ private:
+  int numDoors;
 
-    public:
-        string toString() {
-            return "Car - Brand: Toyota, Color: Red, Max Speed: 180, Doors: 4";
-        }
+ public:
+  Car(string brand, string color, int speed, int numDoors) {
+    this->brand = brand;
+    this->color = color;
+    this->speed = speed;
+    this->numDoors = numDoors;
+  }
+
+  string toString() {
+    return "Car - Brand: Toyota, Color: Red, Max Speed: 180, Doors: 4";
+  }
 };
 
 int main() {
-    return 0;
+  //   Car carObj1("BMW", "X5", 1999);
+  //   Car carObj2("Ford", "Mustang", 1969);
+
+  return 0;
 }
