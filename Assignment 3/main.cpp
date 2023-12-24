50c4354c5e55c7a1e31278aafe8c3e861099cfc9
Sarai Vargas <vargas1sarai@gmail.com>
2023-12-23 17:09:12 -0800

    All done :)

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -3,35 +3,76 @@
 using namespace std;
 
 class Vehicle {
- protected:
-  string brand;
-  string color;
-  int speed;
+   protected:
+    string brand;
+    string color;
+    int speed;
 
- public:
-  virtual string toString() = 0;
+   public:
+    virtual string toString() = 0;
 };
 
 class Car : public Vehicle {
- private:
-  int numDoors;
+   private:
+    int numDoors;
 
- public:
-  Car(string brand, string color, int speed, int numDoors) {
-    this->brand = brand;
-    this->color = color;
-    this->speed = speed;
-    this->numDoors = numDoors;
-  }
+   public:
+    Car(string brand, string color, int speed, int numDoors) {
+        this->brand = brand;
+        this->color = color;
+        this->speed = speed;
+        this->numDoors = numDoors;
+    }
 
-  string toString() {
-    return "Car - Brand: Toyota, Color: Red, Max Speed: 180, Doors: 4";
-  }
+    string toString() {
+        return "Car - Brand: " + brand + ", Color: " + color + ", Max Speed: " + to_string(speed) + ", Doors: " + to_string(numDoors);
+    }
+};
+
+class Bike : public Vehicle {
+   private:
+    string type;
+
+   public:
+    Bike(string brand, string color, int speed, string type) {
+        this->brand = brand;
+        this->color = color;
+        this->speed = speed;
+        this->type = type;
+    }
+
+    string toString() {
+        return "Bike - Brand: " + brand + ", Color: " + color + ", Max Speed: " + to_string(speed) + ", Type: " + type;
+    }
+};
+
+class Truck : public Vehicle {
+   private:
+    string loadCapacity;
+
+   public:
+    Truck(string brand, string color, int speed, string loadCapacity) {
+        this->brand = brand;
+        this->color = color;
+        this->speed = speed;
+        this->loadCapacity = loadCapacity;
+    }
+
+    string toString() {
+        return "Truck - Brand: " + brand + ", Color: " + color + ", Max Speed: " + to_string(speed) + ", Load Capacity: " + loadCapacity;
+    }
 };
 
 int main() {
-  //   Car carObj1("BMW", "X5", 1999);
-  //   Car carObj2("Ford", "Mustang", 1969);
+    Vehicle* vehicles[3] = {
+        new Car("Toyota", "Red", 180, 4),
+        new Bike("Trek", "Blue", 60, "Mountain"),
+        new Truck("Ford", "Black", 140, "5000 lb")};
 
-  return 0;
+    for (int i = 0; i < 3; i++) {
+        Vehicle* vehicle = vehicles[i];
+        cout << vehicle->toString() << endl;
+    }
+
+    return 0;
 }
